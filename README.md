04e89d0261785c4852bfaf1a314c54395c5bfa4b
Gcaufy <gcaufy@gmail.com>
2017-12-26 16:08:08 +0800

    first init

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# wepy_templates
-Templates collection
+# WePY Templates collection
+
+templates sources for WePY
+
+
+## Usage
+
+```
+$ wepy init ${template-name} myfolder
+```
+
+## Template List
+
+| Name | Description |
+| --- | --- |
+| [empty](https://github.com/wepyjs/wepy_templates/tree/master/templates/empty) | 空模板 |
+| [standard](https://github.com/wepyjs/wepy_templates/tree/master/templates/empty) | 模认模板，开启了promise、Async Function等 |
+
---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const fs          =          require('fs');
+const path        =          require("path");
+const gulp        =          require("gulp");
+const zip        =          require("gulp-zip");
+const gutil   = require("gulp-util");
+
+const getTemplates = () => {
+  let dirData = fs.readdirSync('./templates');
+  return dirData;
+};
+
+gulp.task("default", ["build"]);
+
+
+gulp.task('build', function () {
+  let templates = getTemplates();
+  gutil.log(`${templates.length} templates found`);
+  templates.forEach(template => {
+    gutil.log(`compress template [${template}]`);
+    gulp.src([`templates/${template}/**`, `!templates/${template}/{node_modules,node_modules/**,dist,dist/**}`])
+      .pipe(zip(`${template}.zip`))
+      .pipe(gulp.dest('zips'));
+  })
+});

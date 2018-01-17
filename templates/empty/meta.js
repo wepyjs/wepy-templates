64b66048edcee0e1365e05725ba1b8ce53a43a97
duanlian <505441575@qq.com>
2018-01-18 01:37:54 +0800

    update templates

---
+++ templates/empty/meta.js
@@ -0,0 +1,36 @@
+module.exports = {
+  "helpers": {
+    "if_or": function (v1, v2, options) {
+      if (v1 || v2) {
+        return options.fn(this);
+      }
+      
+      return options.inverse(this);
+    }
+  },
+  "prompts": {
+    "name": {
+      "type": "string",
+      "required": true,
+      "message": "Project name"
+    },
+    "description": {
+      "type": "string",
+      "required": false,
+      "message": "Project description",
+      "default": "A WePY project"
+    },
+    "author": {
+      "type": "string",
+      "message": "Author"
+    },
+    "lint": {
+      "type": "confirm",
+      "message": "Use ESLint to lint your code?"
+    }
+  },
+  "filters": {
+    ".eslintignore": "lint",
+    ".eslintrc.js": "lint"
+  }
+}

1c5e74ad972d42103bdebf0ee8d4de7300f9ae3b
Gcaufy <gcaufy@gmail.com>
2018-03-21 09:46:30 +0800

    Update wepy.config.js

--- templates/standard/template/wepy.config.js
+++ templates/standard/template/wepy.config.js
@@ -19,6 +19,7 @@ module.exports = {
       counter: path.join(__dirname, 'src/components/counter'),
       '@': path.join(__dirname, 'src')
     },
+    aliasFields: ['wepy', 'weapp'],
     modules: ['node_modules']
   },
   compilers: {

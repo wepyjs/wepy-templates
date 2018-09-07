49ed9f5b5a54dc137f66d4301133bc61e304b6b0
Gcaufy <gcaufy@gmail.com>
2018-09-07 17:35:21 +0800

    added mixin test case

--- templates/standard/template/src/mixins/test.js
+++ templates/standard/template/src/mixins/test.js
@@ -1,21 +1,16 @@
-import wepy from 'wepy'
 
-export default class testMixin extends wepy.mixin {
-  data = {
-    mixin: 'This is mixin data.'
-  }
-  methods = {
-    tap () {
-      this.mixin = 'mixin data was changed'
-      console.log('mixin method tap')
+
+export default {
+  data: {
+    mixin: 'MixinText'
+  },
+  methods: {
+    mixintap () {
+      this.mixin = 'MixinText' + (Math.random() + '').substring(3, 7);
+      console.log('mixin method tap');
     }
-  }
-
-  onShow() {
-    console.log('mixin onShow')
-  }
-
-  onLoad() {
-    console.log('mixin onLoad')
+  },
+  created () {
+    console.log('created in mixin');
   }
 }
